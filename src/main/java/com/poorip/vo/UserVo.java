d3041120a7d13d6a18e8b318fa18e7401080c647
BIT <BIT@DESKTOP-3TH130A>
2017-04-10 20:55:35 +0900

    Test/Dao 샘플 완료

--- src/main/java/com/poorip/vo/UserVo.java
+++ src/main/java/com/poorip/vo/UserVo.java
@@ -19,109 +19,178 @@ public class UserVo {
 	private String usrJoinDate;
 	private String usrLoginDate;
 	private String usrBlackYN;
+
 	
 	public String getUsrEmail() {
 		return usrEmail;
 	}
+
+
 	public void setUsrEmail(String usrEmail) {
 		this.usrEmail = usrEmail;
 	}
+
+
 	public String getUsrProfile() {
 		return usrProfile;
 	}
+
+
 	public void setUsrProfile(String usrProfile) {
 		this.usrProfile = usrProfile;
 	}
+
+
 	public String getUsrGender() {
 		return usrGender;
 	}
+
+
 	public void setUsrGender(String usrGender) {
 		this.usrGender = usrGender;
 	}
+
+
 	public String getUsrLang() {
 		return usrLang;
 	}
+
+
 	public void setUsrLang(String usrLang) {
 		this.usrLang = usrLang;
 	}
+
+
 	public String getUsrBd() {
 		return usrBd;
 	}
+
+
 	public void setUsrBd(String usrBd) {
 		this.usrBd = usrBd;
 	}
+
+
 	public String getUsrNick() {
 		return usrNick;
 	}
+
+
 	public void setUsrNick(String usrNick) {
 		this.usrNick = usrNick;
 	}
+
+
 	public String getUsrInfo() {
 		return usrInfo;
 	}
+
+
 	public void setUsrInfo(String usrInfo) {
 		this.usrInfo = usrInfo;
 	}
+
+
 	public String getUsrHashtag() {
 		return usrHashtag;
 	}
+
+
 	public void setUsrHashtag(String usrHashtag) {
 		this.usrHashtag = usrHashtag;
 	}
+
+
 	public String getUsrPref1() {
 		return usrPref1;
 	}
+
+
 	public void setUsrPref1(String usrPref1) {
 		this.usrPref1 = usrPref1;
 	}
+
+
 	public String getUsrPref2() {
 		return usrPref2;
 	}
+
+
 	public void setUsrPref2(String usrPref2) {
 		this.usrPref2 = usrPref2;
 	}
+
+
 	public String getUsrPref3() {
 		return usrPref3;
 	}
+
+
 	public void setUsrPref3(String usrPref3) {
 		this.usrPref3 = usrPref3;
 	}
+
+
 	public String getUsrPref4() {
 		return usrPref4;
 	}
+
+
 	public void setUsrPref4(String usrPref4) {
 		this.usrPref4 = usrPref4;
 	}
+
+
 	public String getUsrPref5() {
 		return usrPref5;
 	}
+
+
 	public void setUsrPref5(String usrPref5) {
 		this.usrPref5 = usrPref5;
 	}
+
+
 	public String getUsrNoti() {
 		return usrNoti;
 	}
+
+
 	public void setUsrNoti(String usrNoti) {
 		this.usrNoti = usrNoti;
 	}
+
+
 	public String getUsrJoinDate() {
 		return usrJoinDate;
 	}
+
+
 	public void setUsrJoinDate(String usrJoinDate) {
 		this.usrJoinDate = usrJoinDate;
 	}
+
+
 	public String getUsrLoginDate() {
 		return usrLoginDate;
 	}
+
+
 	public void setUsrLoginDate(String usrLoginDate) {
 		this.usrLoginDate = usrLoginDate;
 	}
+
+
 	public String getUsrBlackYN() {
 		return usrBlackYN;
 	}
+
+
 	public void setUsrBlackYN(String usrBlackYN) {
 		this.usrBlackYN = usrBlackYN;
 	}
+
+
 	@Override
 	public String toString() {
 		return "UserVo [usrEmail=" + usrEmail + ", usrProfile=" + usrProfile + ", usrGender=" + usrGender + ", usrLang="
