9d7a7a549cbe24fefb28fbf4e519830f3efbb2f4
jin <BIT@DESKTOP-A98GAPS>
2017-05-25 11:33:21 +0900

    dddd

--- src/main/webapp/assets/js/sns-ajax.js
+++ src/main/webapp/assets/js/sns-ajax.js
@@ -42,7 +42,7 @@ var edit_post_render = function( vo ) {
 
 var postPic_render = function(vo2, vo) {
    if(vo2.fileName!=null) {
-   var postPic_html = "<a class='sns-image' href='/poorip" + vo2.path + "/" + vo2.fileName + " 'width='500px' id='middle-html-" + vo2.postPicSeq + "' data-lightbox='sns-images-"+ vo.postSeq+ "' id='middle-html-" + vo.postSeq + "'><img src='/poorip" + vo2.path + "/" + vo2.fileName + " 'width='500px' ></a>";
+   var postPic_html = "<a class='sns-image' href='/poorip" + vo2.path + "/" + vo2.fileName + " 'width='500px' id='middle-html-" + vo2.postPicSeq + "' data-lightbox='sns-images-"+ vo.postSeq+ "' id='middle-html-" + vo.postSeq + "'><img src='/poorip" + vo2.path + "/" + vo2.fileName + " style='width=500px; margin:auto;' ></a>";
                               
 
                   $( "#first-html-"+vo.postSeq ).append(postPic_html);
