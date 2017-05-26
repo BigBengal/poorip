e18ef5c5d374957fabcbabcc28b4356695353ad1
bit-pc <bit-pc@bit-bengal>
2017-05-26 17:02:03 +0900

    그래프까지 완료 순서대로 나오고 점수도 100점 기준으로 바꿈

--- src/main/java/com/poorip/service/MatchingService.java
+++ src/main/java/com/poorip/service/MatchingService.java
@@ -82,14 +82,13 @@ public class MatchingService {
 		for (int x = 0; x < myCityList.size(); x++) {
 			String targetDateFrom = myCityList.get(x).getDateFrom();
 			String targetDateTo = myCityList.get(x).getDateTo();
-			
+
 			Date targetFrom = format.parse(targetDateFrom);
 			Date targetTo = format.parse(targetDateTo);
 
 			int ctySeq = myCityList.get(x).getCtySeq();
-			
+
 			List<ScrapCityVo> scrapCityVo = getUsersScrapInfoByCtySeq(ctySeq, myusrSeq);
-			
 			// 내가 스크랩한 도시를 같이 스크랩 한 사용자 스크랩 정보
 			for (int y = 0; y < scrapCityVo.size(); y++) {
 				if (scrapCityVo.get(y) != null) { // NULL 경우가 있음
@@ -98,59 +97,77 @@ public class MatchingService {
 
 					String usersDateFrom = scrapCityVo.get(y).getDateFrom();
 					String usersDateTo = scrapCityVo.get(y).getDateTo();
-				
+
 					Date usersFrom = format.parse(usersDateFrom);
 					Date usersTo = format.parse(usersDateTo);
 
-					Date maxFrom = new Date();
-					Date minTo = new Date();
+					if( targetFrom.compareTo(usersTo) != 1 && usersFrom.compareTo(targetTo) != 1){
 
-					// 앞에 변수가 크면 1, 작으면 -1, 같으면 0
-					int fromResult = targetFrom.compareTo(usersFrom);
-					int toResult = targetTo.compareTo(usersTo);
+						Date maxFrom = new Date();
+						Date minTo = new Date();
 
-					if (fromResult == 1) {
-						maxFrom = targetFrom;
-					} else if (fromResult == -1) {
-						maxFrom = usersFrom;
-					} else {
-						maxFrom = targetFrom;
-					}
-
-					if (toResult == 1) {
-						minTo = usersTo;
-					} else if (toResult == -1) {
-						minTo = targetTo;
-					} else {
-						minTo = targetTo;
-					}
-
-					long diff = Math.abs(minTo.getTime() - maxFrom.getTime());
-					long overlapDays = diff / (24 * 60 * 60 * 1000);
-					overlapDaysScore = (int) (overlapDays ^ 2);
-
-					boolean isExistUsr = false;
-					int listSize = samePlanMember.size();
-					// 유저가 이미 리스트에 추가되어 있으면 값을 더해주고
-					for (int z = 0; z < listSize; z++) {
-						if (matchingUsrSeq == samePlanMember.get(z).getUsrSeq()) {
-							int originalScore = samePlanMember.get(z).getDateScore();
-							samePlanMember.get(z).setDateScore(originalScore + overlapDaysScore);
-							isExistUsr = true;
+						// 앞에 변수가 크면 1, 작으면 -1, 같으면 0
+						int fromResult = targetFrom.compareTo(usersFrom);
+						int toResult = targetTo.compareTo(usersTo);
+						if (fromResult == 1) {
+							maxFrom = targetFrom;
+						} else if (fromResult == -1) {
+							maxFrom = usersFrom;
+						} else {
+							maxFrom = targetFrom;
 						}
-					}
-					// 리스트에 없으면 리스트에 추가
-					if (!isExistUsr) {
-						dateScore.setUsrSeq(matchingUsrSeq);
-						dateScore.setDateScore(overlapDaysScore);
-						samePlanMember.add(dateScore);
-					}
-				} // NULL 경우가 있음
+
+						if (toResult == 1) {
+							minTo = usersTo;
+						} else if (toResult == -1) {
+							minTo = targetTo;
+						} else {
+							minTo = targetTo;
+						}
+
+						long diff = Math.abs(minTo.getTime() - maxFrom.getTime());
+						int overlapDays = (int)(diff / (24 * 60 * 60 * 1000));
+						overlapDaysScore = (int) Math.pow(overlapDays, 2);
+						//					System.out.println(scrapCityVo.get(y).getUsrSeq()+"  :  "+overlapDaysScore);
+
+						boolean isExistUsr = false;
+						int listSize = samePlanMember.size();
+						// 유저가 이미 리스트에 추가되어 있으면 값을 더해주고
+						for (int z = 0; z < listSize; z++) {
+							if (matchingUsrSeq == samePlanMember.get(z).getUsrSeq()) {
+								int originalScore = samePlanMember.get(z).getDateScore();
+								samePlanMember.get(z).setDateScore(originalScore + overlapDaysScore);
+								isExistUsr = true;
+							}
+						}
+						// 리스트에 없으면 리스트에 추가
+						if (!isExistUsr) {
+							dateScore.setUsrSeq(matchingUsrSeq);
+							dateScore.setDateScore(overlapDaysScore);
+							samePlanMember.add(dateScore);
+						}
+
+					} // NULL 경우가 있음
+
+				}
 
 			} // for(int y=0; y < scrapCityVo.size(); y++){
 
 		} // for(int x=1; x < myCityList.size(); x++)
-		System.out.println("이거렁야ㅑ러어어ㅏㅏ아"+samePlanMember);
+		float memberCompareToDate = 0;
+		int AllmemberSumDate = 0;
+		for(int i=0; i<samePlanMember.size(); i++) {
+			int dateSocre = samePlanMember.get(i).getDateScore();
+			AllmemberSumDate += dateSocre;
+		}
+
+		for(int j=0; j<samePlanMember.size(); j++) {
+			int dateScore = samePlanMember.get(j).getDateScore();
+			memberCompareToDate = (float)((float)dateScore/(float)AllmemberSumDate);
+			int dateSocre = (int)(memberCompareToDate * 100);
+			samePlanMember.get(j).setDateScore(dateSocre);
+		}
+		System.out.println(samePlanMember);
 		return addUsrOtherInfo(samePlanMember);
 	}
 
@@ -209,13 +226,13 @@ public class MatchingService {
 						surveyScore = surveyScore + 20; 
 					if(myInfo.getUsrPref5().equals(userScore.getUsrPref5()))
 						surveyScore = surveyScore + 20;
-					
+
 					userScore.setUseriSurveyScore(surveyScore);
 				} else {
 					userScore.setUsrSeq( usrSeq_survey );
 					userScore.setUseriSurveyScore(surveyScore);
 				}
-				
+
 				int foodScore = 0;
 				int sightScore = 0;
 				int activityScore = 0;
@@ -223,12 +240,12 @@ public class MatchingService {
 
 				if(foodHit != 0 && sightHit != 0 && activityHit != 0 && luxuryHit != 0 && sumHit != 0 
 						&& myFoodHit != 0 && mySightHit != 0 && myActivityHit != 0  && myLuxuryHit != 0 && mySumHit != 0){
-					
+
 					//---------------------------------음식점 점수 계산-----------------------------------------------------			
 					float sumFood = (1-Math.abs(((float)foodHit/(float)sumHit)-((float)myFoodHit/(float)mySumHit)));
 					foodScore = (int)(sumFood*100);
 					userScore.setFoodScore(foodScore);
-					
+
 					//---------------------------------관광지 점수 계산-----------------------------------------------------
 					float sumSight = (1-Math.abs(((float)sightHit/(float)sumHit)-((float)mySightHit/(float)mySumHit)));
 					sightScore = (int)(sumSight*100);
@@ -282,14 +299,19 @@ public class MatchingService {
 
 	// List 정렬
 	private List<UserVo> getTopMatchingMember(List<UserVo> matchingScoreList) {
-		// 내림차순 정렬
+
 		Descending descending = new Descending();
+		Collections.sort(matchingScoreList, descending);
+
 		int mininum = matchingScoreList.size();
 		mininum = Math.min(mininum, 5);
 		List<UserVo> top5 = matchingScoreList.subList(0, mininum);
-		Collections.sort(top5, descending);
+
 		return addUsrOtherInfo( top5 );
 	}
+
+
+
 
 	public List<UserVo> addUsrOtherInfo(List<UserVo> samePlanMember) {
 		return matchingDao.addUsrOtherInfo( samePlanMember );
